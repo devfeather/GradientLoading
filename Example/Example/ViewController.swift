91a3aa0296c2cd50d9611d93a536afd5016f8a91
feather <dev.thefeather@gmail.com>
2017-09-08 13:21:21 +0900

    예제 코드 추가.

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -9,18 +9,33 @@
 import UIKit
 import GradientLoading
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CAAnimationDelegate {
 
+    @IBOutlet weak var loadingButton: UIButton! {
+        didSet {
+            loadingButton.layer.cornerRadius = 5.0
+            loadingButton.layer.borderWidth = 1.0
+            loadingButton.layer.borderColor = UIColor.white.cgColor
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup Text, Font, Stroke Color, Stroke Width.
+        GradientLoading.shared.setFontAttributes(text: "Jennie")
+        
+        // use setCollectionColors or setColors.
+        GradientLoading.shared.setCollectionColors(.aurora)
+        // GradientLoading.shared.setColors([#colorLiteral(red: 0.662745098, green: 0.7882352941, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 0.4156862745, blue: 0.5333333333, alpha: 1), #colorLiteral(red: 1, green: 0.7333333333, blue: 0.9254901961, alpha: 1), #colorLiteral(red: 0.9333333333, green: 0.4549019608, blue: 0.8823529412, alpha: 1)])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showLoading(_ sender: Any) {
+        // Show Gradient Loading
+        GradientLoading.shared.show()
+        
+        // Hide after 5 second
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            GradientLoading.shared.hide()
+        }
     }
-
-
 }
-
